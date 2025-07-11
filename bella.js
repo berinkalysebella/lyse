9147ff36dd77ed9f9b057bfbe06428b6f0e80475
Berinka Lyse Bella <bellalyseberinka@gmail.com>
2025-07-11 06:36:28 -0700

    ajout de bella javascript

---
+++ bella.js
@@ -0,0 +1,48 @@
+// let myAge = 30
+// console.log(myAge)
+
+// let myDetails = {
+//     name: 'bella',
+//     age: 22,
+//     gender: 'femal',
+
+// }
+
+// myDetails.job= 'student'
+
+// console.log(myDetails.job)
+
+// let cinema= {
+//     nomFilm: 'All American',
+//     prix : 50000,
+//     numeroSalle: '5A'
+// }
+// console.log(cinema)
+
+// cinema.nom='Berinka'
+
+// console.log(cinema)
+// console.log(cinema.nom)
+// let  texteAffichage= 'Bonjour ' + cinema.nom +', votre film ' + cinema.nomFilm + ' est en salle ' +  cinema.numeroSalle
+// console.log(texteAffichage)
+
+// let playlist = ['the old i get', 'oceans', 'mille reasons']
+// let totalMorceaux = playlist.length
+
+// playlist.push('Reckless Love', 'Holy Forever')
+// playlist.pop()
+// console.log(playlist)
+// console.log(totalMorceaux)
+
+// let mottapok = false
+// if ( mottapok){
+//     console.log('bravo you succed')
+// }else{
+//     console.log('boum it is not true')
+
+// }
+let i = 0
+while (i < 3) {
+    console.log(i)
+    i++
+}
